dfb933d1c9e7e1f9aa368ed9f6c48046bd6d5541
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2024-05-08 16:37:23 -0300

    feat (web): redirect to questions page button

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { useNavigate } from "react-router-dom";
 function Home() {
-    return <h1>This is a Home Page</h1>
+    const navigate = useNavigate();
+    return (
+        <>
+            <h1>Terms of agreement</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis unde at ipsum, recusandae porro commodi dolore, dolorum eum iure odit facilis totam eaque aliquam, deleniti fuga odio? Asperiores, quam quaerat.</p>
+            <button onClick={() => navigate("/questions")}>I agree</button>
+        </>
+    )
 }
 
 export default Home;
