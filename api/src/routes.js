bf7eee17f1daf643dc485c3281c14d4bde48915d
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2024-05-10 17:35:20 -0300

    feat (api): import controller on getQuestions endpoint

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const helmet = require('helmet');
 const routes = express.Router();
 
+const GetQuestionsController = require('./controllers/QuestionsControllers/GetQuestionsController.js');
+
 const safeList = [
     'http://localhost:3000'
 ];
@@ -21,34 +23,7 @@ const corsOptions = {
 routes.use(cors(corsOptions));
 routes.use(helmet());
 
-// // NOTE: Boilerplate to test connection
-// const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize('questions_db', 'postgres', 'postgres', {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
-
-routes.get('/questions', async (req, res) => {
-    try {
-        // await sequelize.authenticate();
-        // console.log('Connection has been established successfully.');
-        const mockQuestions = [
-            {
-                questionTitle: 'question 01',
-                questionContent: 'lorem ipsum dolor sit amet, consectetur adip',
-                questionOptionsType: 'simple',
-                questionOptions: [{ title: 'lorem1' }, { title: 'lorem2' }, { title: 'lorem3' }, { title: 'lorem4' }]
-            }, {
-                questionTitle: 'question 02',
-                questionContent: 'lorem olx bando de ***, ',
-                questionOptionsType: 'simple',
-                questionOptions: [{ title: 'lorem21' }, { title: 'lorem22' }, { title: 'lorem23' }, { title: 'lorem24' }]
-            }];
-        return res.status(200).json(mockQuestions);
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-});
+routes.get('/questions', GetQuestionsController);
 
 routes.post('/questions', async (req, res) => {
     try {
